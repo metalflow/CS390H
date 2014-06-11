adc2bc5c2705b82d636fae4d01ed07445d088bcf
metalflow <metal.flow@gmail.com>
2014-06-11 05:59:48 -0600

    Project ready for submission

--- ProjectSelector/src/edu/msudenver/CS390H/assignment1Activity.java
+++ ProjectSelector/src/edu/msudenver/CS390H/assignment1Activity.java
@@ -3,46 +3,115 @@ package edu.msudenver.CS390H;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.TextureView.SurfaceTextureListener;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
 import android.view.*;
 import android.graphics.*;
 
 public class assignment1Activity extends Activity {
-	
+
 	private static NumberPicker redNP = null;
 	private static NumberPicker greenNP = null;
 	private static NumberPicker blueNP = null;
-	private static SurfaceView colorSurface=null;
-	private static Canvas colorCanvas=null;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.assignment1);
-        
-        redNP = (NumberPicker) findViewById(R.id.redNumberPicker);
-        greenNP = (NumberPicker) findViewById(R.id.greenNumberPicker);
-        blueNP = (NumberPicker) findViewById(R.id.blueNumberPicker);
-        
+	private static ImageView drawingImageView = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.assignment1);
+
+		redNP = (NumberPicker) findViewById(R.id.redNumberPicker);
+		greenNP = (NumberPicker) findViewById(R.id.greenNumberPicker);
+		blueNP = (NumberPicker) findViewById(R.id.blueNumberPicker);
+
 		redNP.setMinValue(0);
 		redNP.setMaxValue(255);
 		greenNP.setMinValue(0);
 		greenNP.setMaxValue(255);
 		blueNP.setMinValue(0);
 		blueNP.setMaxValue(255);
-		
-		colorSurface = (SurfaceView) findViewById(R.id.colorView);
-		colorCanvas = new Canvas();
-		
-        Log.i("Assignemtn1_OnCreate", "On create finished");
-        
-    }
 
-    public void updateColor(View view){
-		colorCanvas.drawARGB(0,redNP.getValue(),greenNP.getValue(),blueNP.getValue());
-		colorSurface.draw(colorCanvas);
-    	Log.i("Assignemtn1_updateColor", "surface has been clicked");
-    }
+		drawingImageView = (ImageView) this.findViewById(R.id.DrawingImageView);
+		Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+				.getDefaultDisplay().getWidth(), (int) getWindowManager()
+				.getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		drawingImageView.setImageBitmap(bitmap);
+
+		// Rectangle
+
+		Paint paint = new Paint();
+		paint.setColor(Color.rgb(redNP.getValue(), greenNP.getValue(),
+				blueNP.getValue()));
+		paint.setStyle(Paint.Style.FILL_AND_STROKE);
+		paint.setStrokeWidth(10);
+		float leftx = 0;
+		float topy = 0;
+		float rightx = bitmap.getWidth();
+		float bottomy = bitmap.getHeight();
+
+		canvas.drawRect(leftx, topy, rightx, bottomy, paint);
+
+		redNP.setOnValueChangedListener(new OnValueChangeListener() {
+			@Override
+			public void onValueChange(NumberPicker picker, int oldVal,
+					int newVal) {
+				Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+						.getDefaultDisplay().getWidth(),
+						(int) getWindowManager().getDefaultDisplay()
+								.getHeight(), Bitmap.Config.ARGB_8888);
+				Canvas canvas = new Canvas(bitmap);
+				drawingImageView.setImageBitmap(bitmap);
+
+				canvas.drawRGB(redNP.getValue(), greenNP.getValue(),
+						blueNP.getValue());
+			}
+		});
+		greenNP.setOnValueChangedListener(new OnValueChangeListener() {
+			@Override
+			public void onValueChange(NumberPicker picker, int oldVal,
+					int newVal) {
+				Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+						.getDefaultDisplay().getWidth(),
+						(int) getWindowManager().getDefaultDisplay()
+								.getHeight(), Bitmap.Config.ARGB_8888);
+				Canvas canvas = new Canvas(bitmap);
+				drawingImageView.setImageBitmap(bitmap);
+
+				canvas.drawRGB(redNP.getValue(), greenNP.getValue(),
+						blueNP.getValue());
+			}
+		});
+		blueNP.setOnValueChangedListener(new OnValueChangeListener() {
+			@Override
+			public void onValueChange(NumberPicker picker, int oldVal,
+					int newVal) {
+				Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+						.getDefaultDisplay().getWidth(),
+						(int) getWindowManager().getDefaultDisplay()
+								.getHeight(), Bitmap.Config.ARGB_8888);
+				Canvas canvas = new Canvas(bitmap);
+				drawingImageView.setImageBitmap(bitmap);
+
+				canvas.drawRGB(redNP.getValue(), greenNP.getValue(),
+						blueNP.getValue());
+			}
+		});
+
+		Log.i("Assignemtn1_OnCreate", "On create finished");
+
+	}
+
+	public void updateColor(View view) {
+		Bitmap bitmap = Bitmap.createBitmap((int) getWindowManager()
+				.getDefaultDisplay().getWidth(), (int) getWindowManager()
+				.getDefaultDisplay().getHeight(), Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		drawingImageView.setImageBitmap(bitmap);
+
+		canvas.drawRGB(redNP.getValue(), greenNP.getValue(), blueNP.getValue());
+	}
 }
