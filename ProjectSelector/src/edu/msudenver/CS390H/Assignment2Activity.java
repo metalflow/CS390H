07f32ae6451b64a605fe4599f5c4e3c96e0f07d7
Cole Risch <metal.flow@gmail.com>
2014-06-16 19:54:02 -0600

    in class work 06/16

--- ProjectSelector/src/edu/msudenver/CS390H/Assignment2Activity.java
+++ ProjectSelector/src/edu/msudenver/CS390H/Assignment2Activity.java
@@ -2,10 +2,13 @@ package edu.msudenver.CS390H;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class Assignment2Activity extends Activity {
 
-	/** Called when the activity is first created. */
+	ArrayAdapter MainList;
+	List Listofstuff;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -13,4 +16,14 @@ public class Assignment2Activity extends Activity {
 	    setContentView(R.layout.assignment2);
 	}
 
+	@Override
+	public void onResume(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+		
+		if (savedInstanceState.get("MainList")==null)
+	    	MainList = new ArrayAdapter(this,findViewById(R.id.mainListView,Listofstuff);
+		else
+			MainList = (ArrayAdapter) savedInstanceState.get("MainList");
+	}
+	
 }
